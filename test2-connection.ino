5d8751013b4ce10c8a53a91dc456c84c26885480
Amira Sayed Mohamed <99423625+AmiraSayedMohamed@users.noreply.github.com>
2025-11-21 23:51:38 +0200

    Add basic Arduino sketch for serial communication

---
+++ test2-connection.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // Serial setup code here to run once
+  Serial.begin(9600);
+  delay(100); 
+}
+
+void loop() {
+  // Put your main code here to run repeatedly
+  Serial.println("HI CMTEQ");
+  delay(1000);
+}
